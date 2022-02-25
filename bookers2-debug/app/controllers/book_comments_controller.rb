128adc26ababba492da3bec2a3576eda4bcd3280
EC2 Default User <ec2-user@ip-172-31-37-178.ec2.internal>
2022-02-25 07:15:34 +0000

    [Finish-add]2-1-3

--- bookers2-debug/app/controllers/book_comments_controller.rb
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -9,11 +9,8 @@ class BookCommentsController < ApplicationController
     end
     
     def destroy
-        book = Book.find(params[:book_id])
-        comment = current_user.book_comments.new(book_comment_params)
-        comment.book_id = book.id
-        comment.destroy
-        redirect_back(fallback_location: root_path)  
+        BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+        redirect_to request.referer
     end
     
     private
--- bookers2-debug/app/models/book_comment.rb
+++ bookers2-debug/app/models/book_comment.rb
@@ -3,4 +3,6 @@ class BookComment < ApplicationRecord
     belongs_to :user
     belongs_to :book
     
+    # check
+    validates :comment, presence: true
 end
